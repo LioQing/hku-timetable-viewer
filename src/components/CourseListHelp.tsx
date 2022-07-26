2f71e52e0f28d76f7e85296acda10d370163ea18
LioQing <46854695+LioQing@users.noreply.github.com>
2022-07-26 16:51:37 +0800

    Improve help messages

--- src/components/CourseListHelp.tsx
+++ src/components/CourseListHelp.tsx
@@ -10,9 +10,7 @@ const CourseListHelp = ({ open, onClose }: Props) => {
   return (
     <Prompt title='Course List Help' open={open} onClose={onClose}>
       <Typography variant='body1'>
-        The 'SELECTED ONLY' button is for filtering the course list to only show the courses you have selected.
-        <br />
-        <br />
+        <strong>Upload/Download</strong><br />
         The upload button is for uploading the class timetable in .xlsx format.
         It assumes the first sheet is the timetable.
         Generally, the class timetables can be found in HKU Portal -&gt; Timetable -&gt; Class Timetable -&gt; Download Class Timetable (for XXXX-XX).
@@ -26,13 +24,16 @@ const CourseListHelp = ({ open, onClose }: Props) => {
         The downloaded file will contain both the complete class timetable and the selected courses, put in separate sheets.
         <br />
         <br />
-        You can use the search bar to search for a course in the course list by its course code and/or class.
+        <strong>Filtering</strong><br />
+        The 'SELECTED ONLY' button is for filtering the course list to only show the courses you have selected.
         <br />
         <br />
-        You can select the semester next to the search bar using the dropdown menu.
+        The search bar and sem dropdown menu can be used to search for courses by the code and the class and filter the semester respectively.
         <br />
         <br />
-        You can also see more detailed info by pressing the info button on the right of the course in the course list.
+        <strong>Course Info</strong><br />
+        The info button next to the courses can be clicked to see more detail of the course,
+        including the title, instructor, offering department, exact timeslots, venues.
       </Typography>
     </Prompt>
   );
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -21,16 +21,25 @@ const Intro = () => {
           This is a simple web app that allows you to view and plan your HKU timetable.
           <br />
           <br />
+          <strong>Getting Started</strong><br />
           Start by uploading the class timetables in .xlsx format.
           Press the help button on the course list to see more info.
           <br />
           <br />
+          <strong>Tab View</strong><br />
+          You can open multiple tabs, where the selected courses will be stored within that tab to make planning for multiple sems or backup plans easier.
+          Double click the tab to rename.
+          <br />
+          <br />
+          <strong>Conflicted Courses</strong><br />
           When multiple courses have a conflicted time slot, you can hover over the time slot to see which courses are conflicted.
           <br />
           <br />
+          <strong>Options</strong><br />
           You can select the hour range and days you want to view in the timetable at panel below the course list.
           <br />
           <br />
+          <strong>Bugs and Feedbacks</strong><br />
           Please report any bug, or provide feedback to me at the GitHub repo. The button below links to the repo page.
         </Typography>
         <Button
